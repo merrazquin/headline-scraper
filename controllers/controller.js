53bfcb25e4f8975cfa234f9168e1a0d7a3bd0c3e
Marcela Errazquin <merrazquin@gmail.com>
2018-06-15 23:30:49 -0400

    Scraping into DB
    Skipping scrape is there is no new data

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,11 +1,64 @@
 const express = require('express'),
-  router = express.Router()
+    cheerio = require('cheerio'),
+    request = require('request'),
+    router = express.Router(),
+    mongoose = require('mongoose'),
+    BASE_URL = 'https://xkcd.com',
+    Headline = mongoose.model('Headline', mongoose.Schema({
+        title: String,
+        link: String,
+        imgURL: String
+    }))
 
-// Create all our routes and set up logic within those routes where required.
+
 router.get('/', (req, res) => {
-    res.render('index', {})
-})
 
+    // pull HTML 
+    request(BASE_URL + '/archive/', (err, response, html) => {
+        if (err) {
+            return res.render('error', { error: err.toString() })
+        }
+
+        let $ = cheerio.load(html),
+            results = []
+
+        Headline.findOne({ link: $('#middleContainer a')[0].attribs.href }, (err, headline) => {
+            if (err) {
+                return res.render('error', { error: err })
+            }
+
+            if (!headline) {
+                // new data, perform full scrape
+                $('#middleContainer a').each((i, element) => {
+                    let title = $(element).text(),
+                        link = $(element).attr('href')
+
+                    Headline.findOneAndUpdate(
+                        { link: link }, // link should be unique, so search by that
+                        { $set: { title: title } }, // update the title if necessary
+                        { upsert: true, new: true, setDefaultsOnInsert: true }, // upsert - insert or update depending on existence
+                        (error, result) => {
+                            if (error) {
+                                return console.error(error)
+                            }
+                        })
+
+                    results.push({ title: title, link: link })
+                })
+                res.render('index', { base: BASE_URL, headlines: results })
+            } else {
+                // no new data, pull existing items from DB
+                Headline.find({}, null, { sort: '_id' }, (err, headlines) => {
+                    if (err) {
+                        return res.render('error', { error: err })
+                    }
+
+                    res.render('index', { base: BASE_URL, headlines: headlines })
+                })
+            }
+        })
+    })
+})
 
 // Export routes for server.js to use.
 module.exports = router
